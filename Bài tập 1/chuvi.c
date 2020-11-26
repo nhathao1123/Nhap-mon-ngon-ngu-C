450db7996b25db915bea8ae5ca81206972f25296
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2020-11-26 17:03:09 +0700

    Create chuvi.c

---
+++ Bài tập 1/chuvi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int made (void) 
+{
+
+ // khai bao bien R kieu double
+ double R ;
+ // khai bao bien R = 2.5
+ double R = 2.5;
+ // khai bao hang ten PI 3.14
+ const double PI = 3.14;
+ double CV , DT ;
+ printf ("R = %.1f,R");
+ printf ("\n");
+ printf ("PI.%.2f",PI)
+ cv = 2*R*PI;
+ DT = R*R*PI;
+ printf ("\n");
+ printf ("2/R/PI\tR*R*PI\n%.1f\t%.1f",CV,DT);
+ return 0;
+}
+ 
