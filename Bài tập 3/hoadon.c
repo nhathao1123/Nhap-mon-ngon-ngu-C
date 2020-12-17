dbe3ca2754adebc86ff24df827aed4dcfd0f98a1
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2020-12-17 14:14:46 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,16 +1,21 @@
-#include <stido.h>
-
-int main () {
-	// khai bao bien dongia , bien soluong , bin thanh tien 
-	int donggia , soluong , thanh tien ;
-	pritnf ("nhap dongia =") ;
-	scanf ("%d",& dongia );
-	pritnf ("nhap so luong = ");
-	scanf  ("%d",& so luong );
-	if (so luong >=5) 
-	thanh tien =(dongia*so luong )*75/100
-    pritnf (%d,thanh tien );
-    
-    
+int main(){
+	int dongia, soluong, thanhtien;
+	printf("nhap don gia :",dongia);
+	scanf("%d",&dongia);
+	printf("nhap soluong :",soluong);
+	scanf("%d",&soluong);
+	thanhtien=dongia*soluong;
+	if(soluong>=5 && dongia>=500) 
+{ 
+thanhtien=(dongia*soluong)*0.5;
+	printf("%d", thanhtien);
+	}
+else
+{
+printf("%d",thanhtien);
+	}
+	getchar();
+	return 0; 
+	}
 	
 	
