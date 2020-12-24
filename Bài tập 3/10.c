8e0caf88dcb1dd7b91119b21128efb2ad6681e9a
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2020-12-24 16:10:36 +0700

    Create 10.c

---
+++ Bài tập 3/10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int   tong = 0;
+  for (int dem =1 ; dem<10;dem++ ){
+    tong = tong+ dem ;
+  }
+  printf ("Tong la :%d",tong);
+    
+   
+
+  return 0;
+}
