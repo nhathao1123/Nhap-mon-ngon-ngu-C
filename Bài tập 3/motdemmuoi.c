a95e59effe63b1cb5ee2d380b3129c0a35e50445
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2020-12-24 14:41:26 +0700

    Create motdemmuoi.c

---
+++ Bài tập 3/motdemmuoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void) {
+	int i ;
+
+	for (i; i<11 ; i++ )
+ 	{
+		printf ("%d ", i) ;
+	}
+    for (i=1; i<11 ; i++)
+    {
+	printf ("%d\n",i );
+	}
+	return 0 ;	
+}
