dc4b3596df98df2aa9169177b90a21b955314183
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2021-01-14 15:39:09 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-int b [4] = {3,5,7,2};  
-for ( int  i = 0 ;i < 4; ++i){
- printf ("%d\n", b[i] );
+int b [4] = {3,5,7,2} ; 
+int c ;
+for (   c = 0 ; c < 4; c++){
+ printf ("%d\n", b[c] );
 }
- int tong =0 ;
- for (int i =0;i<4;++i) {
- 	tong = tong + b[i] ;
+ int tong = 0 ;
+ for ( c = 0 ; c < 4; c++) {
+ 	tong = tong + b[c] ;
  	printf ("tong so = %d\n",tong) ;
  }
  return 0;
