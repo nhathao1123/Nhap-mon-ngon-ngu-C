ce62f14943d470a90e2dfc054ccaf3ffd7a54f50
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2021-01-23 10:48:40 +0700

    Create tong.c

---
+++ Bài tập 2/tong.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf (" cho a= ");
+    scanf("%d", &a); 
+    printf (" cho b= ");
+    scanf("%d", &b);
+    printf ("a + b = %d", a+b);
+    return 0;
+}
