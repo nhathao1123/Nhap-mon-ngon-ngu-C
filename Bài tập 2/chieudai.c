06c393cc03eac42af4c90247fbb484101f75d3c5
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2021-01-23 10:48:08 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int l,m,cm,mm;
+    printf (" cho l = ");
+    scanf("%d", &l);
+    m=l;
+    cm=l*100;
+    mm=l*1000;
+    printf ("m \t cm \t m \n %d \t %d \t %d",m,cm,mm);
+    return 0;
+}
+
+sửa 
+#include <stdio.h>
+int main()
+{
+    int l,m,cm,mm;
+    printf (" cho l = ");
+    scanf("%d", &l);
+    m=l;
+    cm=l*100;
+    mm=l*1000;
+    printf("    ----------------------------------------------------\n \t:met(m)\t        : \t centimet(cm)\t: milimet (mm)\t: \n    ---------------------------------------------------- \n \t: %d \t        :  \t %d \t        : \t %d\t    : \n    ----------------------------------------------------",m,cm,mm);
+    return 0;
+}
