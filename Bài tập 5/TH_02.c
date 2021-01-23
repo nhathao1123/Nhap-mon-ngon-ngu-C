1a3fcab0ffca36c211a0e8a619cb1644ff25ceae
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2021-01-23 09:28:49 +0700

    Create TH_02.c

---
+++ Bài tập 5/TH_02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int k ,a ;
+  int n ; 
+  printf ("cho so de nhap a = ") ;
+  scanf ("%d",&n) ;
+  for ( int i=1;i<=k ;i++ ) {
+    n = n + i ;
+  }
+  printf ("s+1+2+...+ %d",n) ;
+  
+  return 0;
+
+}
