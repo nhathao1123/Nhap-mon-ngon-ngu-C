58b653a4777e010b9c18a5d338268c5134068d8f
nhathao1123 <74343987+nhathao1123@users.noreply.github.com>
2021-03-20 14:33:39 +0700

    Create yeucau2.c

---
+++ Bài tập 5/yeucau2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+int r ;
+float chuvi ; float dientich ;
+float pi = 3.1459 ;
+
+printf ("nhap ban kinh r tu ban phim la  ") ;
+scanf ("%d",&r) ;
+
+chuvi = 2*pi*r  ;
+dientich = pi*r*r ;
+
+printf ("chu vi hinh tron:%f\n",chuvi) ;
+printf ("dien tich hinh trong :%f\n",dientich) ;
+
+
+
+  return 0;
+}
